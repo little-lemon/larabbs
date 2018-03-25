283a1af9fa0d83f1dc5c02adf9fe4c77251b10ef
ying <vagrant@homestead>
2018-03-25 10:18:52 +0000

    Finish basic page layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{--<style type="text/css">
+        .footer {
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            /* Set the fixed height of the footer here */
+            height: 60px;
+            background-color: #000;
+        }
+
+        .footer .container{
+            padding-right: 15px;
+            padding-left: 15px;
+        }
+
+        .footer .container p{
+            margin: 19px 0;
+            color: #c1c1c1;
+        }
+
+        .footer .container a{
+            padding-right: 15px;
+            padding-left: 15px;
+        }
+    </style>--}}
+</head>
+
+<body>
+<div id="app" class="{{ route_class() }}-page">
+
+    @include('layouts._header')
+
+    <div class="container">
+
+        @yield('content')
+
+    </div>
+
+    @include('layouts._footer')
+</div>
+
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
