42599a0b7c09f876f1a63c9f2a32a417106eb94b
ying <vagrant@homestead>
2018-03-30 00:10:54 +0000

    Only authorized user can edit user profile

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -38,8 +38,7 @@
                         </a>
 
                         <ul class="dropdown-menu" role="menu">
-                            
-                            <li>
+                             <li>
                                 <a href="{{ route('users.edit', Auth::id()) }}">
                                     编辑资料
                                 </a>
