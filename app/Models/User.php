14aa01d27776511486c04bb3b3e07a3e3ebe8a38
ying <766019882@qq.com>
2018-05-22 16:11:54 +0000

    topic index

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Http\Requests\Request;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
