127a6a70ff57ce098f39d81911931a2105d5d99f
ying <766019882@qq.com>
2018-05-22 16:37:12 +0000

    replies store

--- routes/api.php
+++ routes/api.php
@@ -76,6 +76,8 @@ $api->version('v1', [
         $api->post('topics', 'TopicsController@store')->name('api.topics.store');
         $api->patch('topics/{topic}', 'TopicsController@update')->name('api.topics.update');
         $api->delete('topics/{topic}', 'TopicsController@destroy')->name('api.topics.destroy');
+        // 发布回复
+        $api->post('topics/{topic}/replies', 'RepliesController@store')->name('api.topics.replies.store');
     });
 
 });
